fd5f2fc3bd22888c9ad04673fba11118f3f4b2e2
Roy Yu <47799668+happyyqt@users.noreply.github.com>
2021-11-26 11:04:18 -0500

    Add files via upload

---
+++ build/precache-manifest.16ef559fca4ef3da5a9a3d356ec20f74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f7fa28af72f03fa7f582ed95c9a65f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19fe673b28af512fba8e",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "19fe673b28af512fba8e",
+    "url": "/static/js/main.b5917b49.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
